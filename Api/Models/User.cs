3a6388f51cd9c03ce67d4eb0343e50c6bd74a98d
netcrawlerr <abdulmejidamk@gmail.com>
2024-11-28 17:09:19 +0300

    "reset code fields"

--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -6,8 +6,11 @@ namespace Api.Models;
 public class User : IdentityUser
 {
     [MaxLength(255)]
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = string.Empty; 
 
     [MaxLength(255)]
-    public string LastName { get; set; }
+    public string LastName { get; set; } = string.Empty;
+
+    public string? ResetCode { get; set; }
+    public DateTime? ResetCodeExpiry { get; set; }
 }
